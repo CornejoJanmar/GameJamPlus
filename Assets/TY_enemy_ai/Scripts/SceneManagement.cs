9c3e1fc1b3614fc81fbec38b50412d763e599eff
Local Pizza Employee <LocalPizzaEmployee@users.noreply.github.com>
2023-10-22 19:01:21 +0800

    Updated Fix Conflict Errors

---
+++ Assets/TY_enemy_ai/Scripts/SceneManagement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false; // delete when building the game
+    }
+}
